8f3c5620b8af3d43fcdc4e1503b82462d52d2c7f
James Asefa <james.asefa@sk3w.co>
2019-12-06 11:40:29 +0000

    chore(funcs): re-add vector functions

--- slsqp/src/test/java/com/example/slsqp/NativeUtilsTest.java
+++ slsqp/src/test/java/com/example/slsqp/NativeUtilsTest.java
@@ -3,7 +3,6 @@ package com.example.slsqp;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class NativeUtilsTest
